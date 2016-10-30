414a069c2617b1913b0c3897fd669b3103370bf1
aravinda <aravinda.misra@sjsu.edu>
2016-10-29 18:32:56 -0700

    print-table

--- user-creation.php
+++ user-creation.php
@@ -88,11 +88,36 @@ else  if ($_POST['action'] == 'Search')
     $stmt->execute();
 
     // set the resulting array to associative
-   $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $result = $stmt->fetchAll();
+   //$stmt->setFetchMode(PDO::FETCH_ASSOC);
+    //$result = $stmt->fetchAll();
+    //Print_r($result);
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Firstname</th><th>Lastname</th><th>Email</th><th>Home Address</th><th>Home Phone</th><th>Mobile Phone</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
 
 
-     print_r($result);
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+
     }
 
 catch(PDOException $e) {
