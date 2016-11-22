36b09fb3153bcb0b0a2cc2e49098639e7678a118
aravinda <aravinda.misra@sjsu.edu>
2016-11-22 11:05:33 -0800

    Individual product

--- product-json.php
+++ product-json.php
@@ -6,6 +6,11 @@ $password = "Welcome01@";
 //$password = "";
 $dbname = "aravinda";
 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+if (empty($_SERVER['PATH_INFO'])){
 try
 {
  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -13,6 +18,8 @@ try
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $stmt = $conn->prepare("SELECT product_id , product_name ,quantity,product_image,product_cost,product_type   FROM  product where 1");
+
+
 
     $stmt->execute();
 
@@ -39,8 +46,59 @@ echo json_encode($result);
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
+
+}
+else
+{
+try{
+
+
+$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+$table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
+
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("SELECT product_id , product_name ,quantity,product_image,product_cost,product_desc   FROM  product where 1 and product_id = :value");
+
+
+
+    $stmt->bindParam(':value', $table);
+
+    $stmt->execute();
+
+
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+$result = $stmt->fetchAll();
+
+$result = array_map(function($result) {
+    return array(
+        'id' => $result['product_id'],
+        'name' => $result['product_name'],
+        'quantity' => $result['quantity'],
+        'image_url' => $result['product_image'],
+                'price' => $result['product_cost'],
+        'description' => $result['product_desc'],
+    );
+}, $result);
+
+echo json_encode($result);
+
+
+
+}
+
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+}
+
+
+
+
 $conn = null;
-
-
 
 ?>
