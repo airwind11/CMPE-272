8177675719726cdad0d1498ba3e453d214740265
aravinda <aravinda.misra@sjsu.edu>
2016-12-05 20:54:32 -0800

    product-quantity -update api updated with update query

--- product-quantity-update.php
+++ product-quantity-update.php
@@ -1,9 +1,33 @@
 <?php
-extract($_POST);
-print_r($_POST);
-foreach ($_POST as $key => $value) {
-    print_r($value);
+
+$servername = "localhost";
+$username = "aravinda";
+$password = "Welcome01@";
+//$username = "root";
+//$password = "";
+$dbname = "aravinda";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $data = (array) json_decode(file_get_contents("php://input"));
+            foreach ($data as $key => $value)
+
+            {
+        $stmt = $conn->prepare("UPDATE product SET quantity= quantity-:order WHERE product_id= :value" );
+                $stmt->bindParam(':value', $key);
+                $stmt->bindParam(':order', $value);
+                $stmt->execute();
+                }
+            }
+
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
+$conn = null;
+
 ?>
 
 
